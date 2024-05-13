12503843422314c85b10b4e11b061b0ce51e11cd
Sascha Szott <szott@gmx.de>
2024-05-13 20:15:16 +0200

    fixed line length

--- dspace-api/src/main/java/org/dspace/core/I18nUtil.java
+++ dspace-api/src/main/java/org/dspace/core/I18nUtil.java
@@ -234,7 +234,8 @@ public class I18nUtil {
         String fileName = "";
         final String FILE_TYPE = ".xml";
         String defsFilename = DSpaceServicesFactory.getInstance().getConfigurationService().getProperty("dspace.dir")
-                              + File.separator + "config" + File.separator + "controlled-vocabularies" + File.separator + vocabularyName;
+                              + File.separator + "config" + File.separator + "controlled-vocabularies"
+                              + File.separator + vocabularyName;
         fileName = getFilename(locale, defsFilename, FILE_TYPE);
         return fileName;
     }
